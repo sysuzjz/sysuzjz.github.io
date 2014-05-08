74fa7eca08da28e82e542b7d0dd2f2f3545ba020
sysuzjz <a1098035@qq.com>
2014-05-08 23:44:48 +0800

    add the vote system

---
+++ vote-system/README.md
@@ -0,0 +1,38 @@
+vote-system
+===========
+
+This project provides a convenient vote system for 2014 election of MIAC. It has no server supports but just a simple front-end web page. Yeah, it's a Single Page Application!! 
+
+This project is in active development, but we strongly recommend you to use it, actually. It's fancy.
+
+## Usage
+It is incredibly easy to use!! Follow the instructions below:
+
+### step 1:
+
+    git clone <git-repo-uri>
+    cd vote-system
+    explore .
+
+### step 2:
+
+Double click the `index.html`, you would see the whole application in your browser. We don't provide any tips for using it, for it is super easy to use; even idiots can figure how to use it without any learnings.
+
+
+## Technical Details
+
+* **jQuery**, and that's all.
+
+## Roadmap
+No roadmaps
+
+## Test
+
+No tests
+
+## Contribution
+We don't accpet any pull requests or any proposals. Forget about it.
+
+## License
+MIT
+
---
+++ vote-system/src/js/main.js
@@ -0,0 +1,111 @@
+$("#main-content").on("click", ".arrow-up", function() {
+    var user = getUser(this);
+    if(isValidUser(user)) {
+        changeVotenum(this, 1);
+    } else {
+        alert("用户名非法");
+    }
+})
+
+$("#main-content").on("click", ".arrow-down", function() {
+    var user = getUser(this);
+    if(isValidUser(user)) {
+        changeVotenum(this, -1);
+    } else {
+        alert("用户名非法");
+    }
+})
+
+$("#main-content").on("click", ".add-user", function() {
+    $("#model").clone(true).removeClass('hide').removeAttr('id').insertBefore($(this)).find(".candidates").trigger('focus');
+})
+
+$("#main-content").on("click", ".btn-close", function() {
+    var user = getUser(this);
+    if(!isValidUser(user) || confirm("确认删除吗？")) {
+        $(this).closest('.vote-box').remove();
+    }
+})
+
+$("#main-content").on("click", ".add-position", function() {
+    var title = prompt("请输入职位名称：", "");
+    if(title) {
+        $("#model-position").clone(true).removeClass('hide').find('.position-title').text(title).closest('.vote-position').removeAttr('id').insertBefore($(this));
+    }
+})
+
+$("#main-content").on("click", ".btn-delete", function() {
+    console.log($(this).closest('.vote-position').find(".position-content").children('div').length);
+    if($(this).closest('.vote-position').find(".position-content").children('div').length === 2 || confirm("确认删除吗？")) {
+        $(this).closest('.vote-position').remove();
+    }
+})
+
+$("#vote-end").click(function(event) {
+    $(".vote-list dl").html("");
+    $(".vote-position").not('.hide').each(function() {
+        var count = 0;
+        var title = $(this).find(".position-title").text();
+        var tempArray = new Array();
+        $(this).find(".vote-box").not('.hide').not('.add-user').each(function(index, el) {
+            var name = $(this).find(".candidates").val();
+            var votenum = parseInt($(this).find(".votenum").val());
+            if(name && votenum) {
+                tempArray[count] = setJSON(name, votenum);
+                count++;
+            }
+        });
+        sortJSON(tempArray);
+        AppendToList(title, tempArray);
+    })
+    $(".vote-list").show(500, "linear");
+});
+
+$("#list-close").click(function() {
+    $(".vote-list").hide(500, "linear");
+})
+
+function getUser(node) {
+    return $(node).siblings('.candidates').val();
+}
+
+function isValidUser(user) {
+    if(!user) {
+        return false;
+    }
+    return true;
+}
+
+function changeVotenum(node, num) {
+    var $numNode = $(node).siblings('.votenum');
+    var currentNum = parseInt($numNode.val());
+    currentNum += num;
+    if(currentNum >= 0) {
+        $numNode.val(currentNum + "票");
+    }
+}
+
+function setJSON(name, votenum) {
+    var jsonStr = '{}';
+    var jsonObj = JSON.parse(jsonStr);
+    jsonObj['name'] = name;
+    jsonObj['votenum'] = votenum;
+    return jsonObj;
+}
+
+function sortJSON(jsonArray) {
+    jsonArray.sort(sortMethod);
+}
+
+function sortMethod(a, b) {
+    return b.votenum - a.votenum;
+}
+
+function AppendToList(title, jsonArray) {
+    var listStr = "<dt>" + title;
+    for(var i = 0; i < jsonArray.length; i++) {
+        listStr = listStr + "<dd>" + jsonArray[i].name + "\t" + jsonArray[i].votenum + "票</dd>";
+    }
+    listStr += "</dt>";
+    $(".vote-list dl").append($(listStr));
+}
